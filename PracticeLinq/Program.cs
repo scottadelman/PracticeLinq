cd9a86ff27d21e2df9ed3035aaddcde6235dedd4
Scott <ScottAdelman97@gmail.com>
2022-07-21 09:30:38 -0600

    Video Games

---
+++ PracticeLinq/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq;
+
+var videoGames = new List<string>() { "call of duty", "halo", "gta", "forza", "rocket league", "fortnite" };
+
+videoGames.OrderBy(x => x.Length)
+          .ToList()
+          .ForEach(x => Console.WriteLine(x));
+
+
+
+
+
